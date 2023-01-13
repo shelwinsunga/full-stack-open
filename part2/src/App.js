88bd6f9deee122815c2e17f367f59529d3da20ba
Shelwin <shelwinsunga@gmail.com>
2023-01-12 22:17:28 -0600

    2.5

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,27 +1,4 @@
-
-const Course = ({ course }) => {
-
-    const Header = ({ course }) => <h1>{course}</h1>
-
-    const Total = ({ parts }) => <p>Number of exercises: {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-
-    const Part = ({ part }) =>
-        <p>
-            {part.name} {part.exercises}
-        </p>
-
-    const Content = ({ parts }) =>
-        <>
-            {parts.map(part => <Part key={part.id} part={part} />)}
-        </>
-    return (
-        <>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </>
-    )
-}
+import Course from "./components/Course"
 
 
 const App = () => {
